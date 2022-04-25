7546e0b111bf19f45d85f387d243bdccff7feac4
sachinkumar53 <kr771.sachin@gmail.com>
2022-04-25 16:47:35 +0530

    tooltip added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const textInput = document.getElementById("text_input");
 const textOutput = document.getElementById("text_output");
-
+const tooltip = document.getElementById("myTooltip");
 
 function convert(){
     const text = textInput.value.trim();
     const lines = Array.from(text.split('\n'));
-    const arr = lines.map(e => e.trim())
-    const joined = arr.join('\\n')
-    textOutput.value = joined
+    const arr = lines.map(e => e.trim());
+    const joined = arr.join('\\n');
+    textOutput.value = joined;
 }
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(textOutput.value);
+    tooltip.innerHTML = "Copied!";
+  }
+  
+function outFunc() {
+    tooltip.innerHTML = "Copy to clipboard";
+  }
